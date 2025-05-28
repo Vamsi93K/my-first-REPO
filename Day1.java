c0c3b0daa66eb3171b481b499679b52991ced522
BAJJURI VAMSI KRISHNA <bajjurivamsikrishna85@gmail.com>
2025-05-28 10:16:10 +0530

    My first Java code upload

---
+++ Day1.java
@@ -0,0 +1,7 @@
+public class Day1 {
+    public static void main(String[] args)
+{
+    System.out.println("helloworld");
+    System.out.println("vamsi krishna");
+    System.out.println("java"); 
+}}
